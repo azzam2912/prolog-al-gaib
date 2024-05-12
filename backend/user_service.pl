df550afef803d687f5c129752a6cf19ead967434
azzam2912 <azzam.gamers12@gmail.com>
2024-05-12 21:20:22 +0700

    edit user-service variable naming

--- backend/user_service.pl
+++ backend/user_service.pl
@@ -3,19 +3,19 @@
 :- use_module(database_service).
 
 % Menambahkan user baru
-add_user(Email, Name, Password, RoleId, Avatar) :-
-    \+ check_existence_from_database(user(Email, _, _, _, _, _, _)), % Pastikan email belum digunakan
+add_user(UserID, Name, Password, RoleId, Avatar) :-
+    \+ check_existence_from_database(user(UserID, _, _, _, _, _, _)), % Pastikan UserID belum digunakan
     get_time(CreatedAt),
-    NewUser = user(Email, Name, Password, CreatedAt, CreatedAt),
+    NewUser = user(UserID, Name, Password, CreatedAt, CreatedAt),
     write_to_database(NewUser).
 
 % Mengedit data user yang sudah ada
-edit_user(Email, NewName, NewPassword, NewRoleId, NewAvatar) :-
+edit_user(UserID, NewName, NewPassword, NewRoleId, NewAvatar) :-
     get_time(UpdatedAt),
-    NewUser = user(Email, NewName, NewPassword, _, UpdatedAt),
-    retractall(user(Email, _, _, _, _)),
+    NewUser = user(UserID, NewName, NewPassword, _, UpdatedAt),
+    retractall(user(UserID, _, _, _, _)),
     write_to_database(NewUser).
 
 % Membaca data user
-get_user(Email, User) :-
-    read_all_from_database(user(Email, _, _, _, _), [User]).
+get_user(UserID, User) :-
+    read_all_from_database(user(UserID, _, _, _, _), [User]).
